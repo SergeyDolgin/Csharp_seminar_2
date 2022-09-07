786261f5d378f678b822505e5b5bd3f03c3dddff
Sergey <seregad07@mail.ru>
2022-09-08 01:51:28 +0700

    task10

---
+++ task_10/Program.cs
@@ -0,0 +1,20 @@
+﻿void secondDigit ()
+{
+try
+{
+    Console.Write("Enter three-digits number ");
+    int n = Convert.ToInt32(Console.ReadLine());
+    if (n-100 > 0 && n-1000 < 0)
+    {
+        Console.WriteLine("Second digit is "+((n / 10) % 10));
+    }
+    else 
+    {
+        Console.WriteLine("You have entered not three-digits number");
+    }
+}
+    catch
+    {
+   }
+}
+   secondDigit ();

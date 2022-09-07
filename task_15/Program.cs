a139bacd95c055f7edfbe2ae48d757ceac057ba8
Sergey <seregad07@mail.ru>
2022-09-08 02:20:33 +0700

    task_15 about days of the week

---
+++ task_15/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write ("Enter number a day of the week ");
+int b = Convert.ToInt32 (Console.ReadLine());
+switch(b)
+{
+    case 1:
+        Console.WriteLine("no");
+        break;
+    case 2:
+        Console.WriteLine("no");
+        break;
+    case 3:
+        Console.WriteLine("no");
+        break;
+    case 4:
+        Console.WriteLine("no");
+        break;
+    case 5:
+        Console.WriteLine("no");
+        break;
+    case 6:
+        Console.WriteLine("yes");
+        break;
+    case 7:
+        Console.WriteLine("yes");
+        break;
+    default:
+        Console.WriteLine("wrong number of a day of the week");
+        break;
+}
